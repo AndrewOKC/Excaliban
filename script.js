ccba7ea512adc9a9779db0ee1a08050c42fcfba0
Andrew White <AndrewWhiteOKC@outlook.com>
2025-02-28 21:48:41 -0600

    PWA

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Register Service Worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+            .then(registration => {
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            })
+            .catch(error => {
+                console.log('ServiceWorker registration failed: ', error);
+            });
+    });
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     // DOM elements
     const board = document.getElementById("board");
